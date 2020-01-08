9e2523f7354566e0cbb5df97387348fff572c20d
bobby mcpherson <bobmcphe@gmail.com>
2020-01-08 16:33:48 -0600

    fixed bug preventing full code running, added total score increment

--- js/app.js
+++ js/app.js
@@ -1,92 +1,97 @@
  'use strict';
-// console.log('working')
-// /*
+console.log('working')
+/*
 
-// if( setTimeout >){
+if( setTimeout >){
   
-//   }
+  }
 
 
-// function = myfunction() {
-//     var delayInMilliseconds = 1000; //1 second
+function = myfunction() {
+    var delayInMilliseconds = 1000; //1 second
 
-// setTimeout(function() {
-//   //your code to be executed after 1 second
-// }, delayInMilliseconds);
+setTimeout(function() {
+  //your code to be executed after 1 second
+}, delayInMilliseconds);
 
-// //timeout code taken from StackOverflow, https://stackoverflow.com/questions/17883692/how-to-set-time-delay-in-javascript
+//timeout code taken from StackOverflow, https://stackoverflow.com/questions/17883692/how-to-set-time-delay-in-javascript
 
-// var name = prompt('What is your name?');
-// alert('Hello, ' + name);
-// }
+var name = prompt('What is your name?');
+alert('Hello, ' + name);
+}
 
-// myFunction();
+myFunction();
 
-// */
+*/
 
-// // Question series
-
-// var name = prompt('What is your name?');
-// alert('Hello ' + name);
-// //  console.log(name);
-
-
-// var play = prompt('This is a guessing game, to try to learn a little about the author, are you ready to play?')
-// if(play.toUpperCase() === 'YES' || play.toUpperCase() ==='Y'){
-//   alert('Great!');
-// } else {
-//   alert('Well, let\'s go ahead, I think you\'ll enjoy it.');
-
-// //  console.log(play);
+// Question series
+var totalScore = 0;
+var name = prompt('What is your name?');
+alert('Hello ' + name);
+//  console.log(name);
 
 
-
-// var book = prompt('Do you think Bobby loves to read & collect books? As in over $3000 spent last year on them?');
-
-// if(book.toUpperCase() === 'YES' || book.toUpperCase() === 'Y'){
-//   alert('Yes! You are right!');
-// } else {
-//   alert('Actually, believe it or not, he is pretty big into books.');
-// }
-// //  console.log(book);
+var play = prompt('This is a guessing game, to try to learn a little about the author, are you ready to play?')
+if(play.toUpperCase() === 'YES' || play.toUpperCase() ==='Y'){
+  alert('Great!');
+} else {
+  alert('Well, let\'s go ahead, I think you\'ll enjoy it.');
+}
+//  console.log(play);
 
 
-// var school = prompt('Does Bobby have more than 3 degrees?');
 
-// if(school.toLowerCase() === 'no' || school.toLowerCase() === 'n'){
-//   alert('You are right; it seems that you are very astute.');
-// } else {
-//   alert('That would be pretty interesting, but I am afraid it is not the case.');
-// }
-// //  console.log(school);
+var book = prompt('Do you think Bobby loves to read & collect books? As in over $3000 spent last year on them?');
 
-// var married = prompt('Do you think Bobby is married?');
+if(book.toUpperCase() === 'YES' || book.toUpperCase() === 'Y'){
+  alert('Yes! You are right!');
+  totalScore++;
+} else {
+  alert('Actually, believe it or not, he is pretty big into books.');
+}
+//  console.log(book);
 
-// if(married.toLowerCase() === 'yes' || married.toLowerCase() === 'y' ){
-//   alert('You are correct');
-// } else{
-//   alert('He is married, in fact.');
-// }
-// // console.log(married);
 
-// var siblings = prompt('Do you think Bobby has any siblings?')
-// if(play.toUpperCase() === 'YES' || play.toUpperCase() === 'Y'){
-//   alert('Indeed, that is correct');
-// } else {
-//   alert('Actually, he does have a brother.');
-// }
-// }
-// //  console.log(siblings);
+var school = prompt('Does Bobby have more than 3 degrees?');
 
-// var outdoors = prompt('Does Bobby love the outdoors')
-// if(outdoors.toUpperCase() === 'YES' || play.toUpperCase() === 'Y'){
-//   alert('That is correct. Sadly, he has little time for it.');
-// } else {
-//   alert('Bobby is actually an avid outdoorsmans, but sadly has no time for it.');
-// }
-// //  console.log(outdoors);
+if(school.toLowerCase() === 'no' || school.toLowerCase() === 'n'){
+  alert('You are right; it seems that you are very astute.');
+  totalScore++;
+} else {
+  alert('That would be pretty interesting, but I am afraid it is not the case.');
+}
+//  console.log(school);
 
-// alert('Thank you, ' + name 'for playing!');
+var married = prompt('Do you think Bobby is married?');
+
+if(married.toLowerCase() === 'yes' || married.toLowerCase() === 'y' ){
+  alert('You are correct');
+  totalScore++;
+} else{
+  alert('He is married, in fact.');
+}
+// console.log(married);
+
+var siblings = prompt('Do you think Bobby has any siblings?')
+if(play.toUpperCase() === 'YES' || play.toUpperCase() === 'Y'){
+  alert('Indeed, that is correct');
+  totalScore++;
+} else {
+  alert('Actually, he does have a brother.');
+}
+
+//  console.log(siblings);
+
+var outdoors = prompt('Does Bobby love the outdoors')
+if(outdoors.toUpperCase() === 'YES' || play.toUpperCase() === 'Y'){
+  alert('That is correct. Sadly, he has little time for it.');
+  totalScore++;
+} else {
+  alert('Bobby is actually an avid outdoorsmans, but sadly has no time for it.');
+}
+//  console.log(outdoors);
+
+alert('Thank you, ' + name + ' for playing! Just a few more quesitons.');
 
 // A 4-attempt number guessing game.
 var myArray = [0,1,2,3,4,5,6,7,8,9,10];
@@ -97,6 +102,7 @@ var numOfGuesses = 1;
     var guess = prompt('What number am I thinking of?');
     if(num == guess) {
       alert('good job!');
+      totalScore++;
       break;
     } else if(guess > num) {
       alert('Try a little lower.');
@@ -110,22 +116,24 @@ var numOfGuesses = 1;
 // 6-attempt non-numeric guessing game
 
 var carArray = ['Honda', 'Ford','Dodge', 'Chrysler', 'Toyota', 'Jeep', 'Lexus'];
-var myCar = carArray[0];
+var myCar = carArray[0, 2, 5];
 var carGuesses = 0;
 
   for(carGuesses = 1; carGuesses <=6; carGuesses++) {
-    var carInput = prompt('What kind of car does Bobby own? Choose from Honda, Ford, Dodge, Jeep, Chrysler, Lexus');
+    var carInput = prompt('What kind of car has Bobby owned? Choose from Honda, Ford, Dodge, Jeep, Chrysler, Lexus');
     
     if(carInput.toLowerCase() === myCar.toLowerCase()) {
       alert('good job!');
+      totalScore++;
       // console.log(carInput);
       break;
     } else if(carInput.toLowerCase() !== myCar.toLowerCase()) {
       alert('Try again.');
       console.log(carInput);
     }
- 
   }
+
+  alert('Your total score is ' + totalScore);
 
 // var userScore = 0;
 // var questions = [0,1,2,3,4,5,6,7,8,9,10];
@@ -140,4 +148,4 @@ var carGuesses = 0;
 //     console.log(userScore);
 //   } else {
 //     alert('That is incorrect');
-//   } }
+//   }
